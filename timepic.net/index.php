1b92e14f173ea23e6d0532f98274982aaee05e06
Will <lishuzu@gmail.com>
2013-09-15 00:08:52 +0800

    debug

--- timepic.net/index.php
+++ timepic.net/index.php
@@ -1,12 +1,12 @@
 <?php
-error_reporting(0);
+//error_reporting(0);
 //error_reporting(E_ALL & ~(E_STRICT | E_NOTICE));
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii/framework/yiilite.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
